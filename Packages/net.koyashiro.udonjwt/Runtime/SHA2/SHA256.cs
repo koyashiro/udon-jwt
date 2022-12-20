c5b67d33d0d06430770c297537255353ea511c85
koyashiro <develop@koyashi.ro>
2022-12-20 23:54:31 +0900

    refactor: use `var`

--- Packages/net.koyashiro.udonjwt/Runtime/SHA2/SHA256.cs
+++ Packages/net.koyashiro.udonjwt/Runtime/SHA2/SHA256.cs
@@ -9,7 +9,7 @@ namespace Koyashiro.UdonJwt.SHA2
 
         public static byte[] ComputeHash(byte[] buffer)
         {
-            uint[] K = new uint[MESSAGE_BLOCK_LENGTH] {
+            var K = new uint[MESSAGE_BLOCK_LENGTH] {
                 0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
                 0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
                 0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
