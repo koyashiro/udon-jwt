b3edf40a4d2c9a74449ef442e23b9a899db586dc
koyashiro <develop@koyashi.ro>
2022-12-28 00:55:56 +0900

    test: update test token

--- Packages/net.koyashiro.udonjwt.tests/Tests/Runtime/DecodeTest.cs
+++ Packages/net.koyashiro.udonjwt.tests/Tests/Runtime/DecodeTest.cs
@@ -13,7 +13,7 @@ namespace Koyashiro.UdonJwt.Tests
 
         public void Start()
         {
-            var token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.c_TDh9O85HFOAska2cM9kN8TGwj2IJkvPtuEc1kBkZM9ZU-UiyypfusznKlPquwUUA00IKfhR1dZDMrG_cNLjPOZ7ocPZL8bWoT2I2gCb-HNG00dNfq44aF-wsomSNn2JL6myJmnIWbPgNP5WOCcJipfsxNloudLGHATLwMM5iGY70YBGF5u9D_Zy7sTTbBJzVAebCos1zgkUIsVxPOwP2zRptOROfKCWB0QCo576ymb41_MTO_kV59VQqwkgk1PLNDsl02BsaMe9CQrTx0_NUtZWox7sPe0rsnVDL2mm4w8vKKgaeXffdzNyEhgXFm6_UjKybXK_Be7u8QddJNN-g";
+            var token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSm9obiBEb2UifQ.Zhe74qVZx0JvMRL7NA-KDWl2wt7T6equyPRZFAdn649Rr-W_DbaVfPPaxuJqt71_LsMgPukEJ9DZfsfEnjJunmruWA1Q1DDXjn66oq-GXQi_mU3rd2DE9Sg3Q-x5h2z8CXNxCj_xGfQwRD1oM0eYJlTfqeGFGQ735EZOzfLpCGCSZIFtFG_4BilHRNsbY-OqfO9Kis6IEvmfkTSa9kn0OBRTCFK8n9xdjFFcYh8KpXU__UqxfcoJBBxfm_RYigZzKxVMVJlEKqFsmNDoKuPjyMsaBhd4ArELSZRKgA9RHYETkLVNP6AP1qavuoV2mfea3SjzUvIkM-3PTuMY8a9EYQ";
             _decoder.Decode(token, this);
         }
 
