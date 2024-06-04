c487e581c867bee6e28edbdc8fd87583f936361c
koyashiro <6698252+koyashiro@users.noreply.github.com>
2024-06-04 22:00:24 +0900

    chore(deps): remove dependency net.koyashiro.udonencoding

--- Packages/net.koyashiro.udonjwt/Runtime/JwtRS256Decoder.cs
+++ Packages/net.koyashiro.udonjwt/Runtime/JwtRS256Decoder.cs
@@ -1,9 +1,9 @@
 using System;
+using System.Text;
 using UdonSharp;
 using UnityEngine;
 using VRC.SDK3.Data;
 using VRC.SDKBase;
-using Koyashiro.UdonEncoding;
 using Koyashiro.UdonJwt.Numerics;
 using Koyashiro.UdonJwt.PKCS1;
 
@@ -133,11 +133,7 @@ namespace Koyashiro.UdonJwt
                 return false;
             }
 
-            if (!UdonUTF8.TryGetString(bytes, out var str))
-            {
-                value = default;
-                return false;
-            }
+            var str = Encoding.UTF8.GetString(bytes);
 
             return VRCJson.TryDeserializeFromJson(str, out value);
         }
@@ -283,7 +279,7 @@ namespace Koyashiro.UdonJwt
         public void _VerifyHash()
         {
             var em = UnsignedBigInteger.ToBytes(_modPowBuf);
-            var emPrime = PKCS1V15Encoder.Encode(UdonUTF8.GetBytes(_tokenHashSource));
+            var emPrime = PKCS1V15Encoder.Encode(Encoding.UTF8.GetBytes(_tokenHashSource));
 
             if (em.Length != emPrime.Length)
             {
